a943ae795d5d3bdaf8ab4d9ea4ee78479d56415e
John Wiggins <johnwiggins96@me.com>
2020-02-20 23:47:13 -0800

    First Function
    Tested out the function feature
    Learned the syntax on how to write functions and reference them in
    python
    One thing to note: use semicolons when initializing the function

---
+++ function_test.py
@@ -0,0 +1,7 @@
+def test_Function(first, last):
+    print("Your name is: " + first + " " + last)
+
+first = input("What is your first name? \n")
+last = input("What is your last name? \n")
+
+test_Function(first, last)
