275706b4f6f5207a8e446aa276726448014c0a13
John Wiggins <johnwiggins96@me.com>
2020-02-25 00:00:00 -0800

    Tested with converting data types
    Testing out changing data types for functions and printing
    Not fully functional yet, committing for storage to test later and
    trouble shoot

--- function_test.py
+++ function_test.py
@@ -1,7 +1,20 @@
+def quick(gallons, cost):
+    gallons = int(gallons)
+    cost = int(cost)
+    total = int(gallons) * int(cost)
+    print(total)
+    print("The total cost of gas is: $" + total)
+
+
 def test_Function(first, last):
     print("Your name is: " + first + " " + last)
 
 first = input("What is your first name? \n")
 last = input("What is your last name? \n")
 
+gallons = input("How many gallons is your tank \n")
+cost = input("How much is gas currently \n ")
+
+
 test_Function(first, last)
+quick(gallons, cost)
