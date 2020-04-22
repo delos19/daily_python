cff3579f49e33a3f7b30079389eba39447d09893
jnwiggins@sdsu.edu <jnwiggins@sdsu.edu>
2020-04-22 00:55:42 -0700

    Added comments and reduced redundant code
    Added comments to explain functions
    Removed lines of code that were redundant or unnecessary, such as bool
    returns

--- pallendrome.py
+++ pallendrome.py
@@ -2,26 +2,26 @@ a = 999
 b = 999
 pal = []
 
+# This function iterates the product of a and b
+# It also appends all pallendromes to the list pal
 def iterateProduct(a, b):
     for i in range(a, 100, -1):
         for j in range(b, 100, -1):
             product = i * j
-            if isPalendrome(product) is True:
-                break
-            else:
-                isPalendrome(product)
+            isPalendrome(product) # sends product to be evaluated
                 
-
-
+# This function determines if the product is a pallendrome
+# Using the product and the reversed() function to
+# determine string equivalence
 def isPalendrome(product):
     product = str(product)
     rev_prod = reversed(product)
     if list(product) == list(rev_prod):
         pal.append(int(product))
-        return True
-    else:
-        return False
 
-iterateProduct(a, b)
-print(pal)
-print('The largest pallendrome is: ', max(pal))
+# Main function for program
+def main(alpha, bravo):
+    iterateProduct(alpha, bravo)
+    print('The largest pallendrome is: ', max(pal))
+
+main(a, b)
