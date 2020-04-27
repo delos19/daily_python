d3bb492cc4b8589c197ae26eb7ad7876f5556d70
jnwiggins@sdsu.edu <jnwiggins@sdsu.edu>
2020-04-26 23:28:26 -0700

    Edited documentation on problem
    Noticed alternative method was not explained
    Elaborated on the difference between the two methods

--- PE_Max_Prime_Alternative_Tree_Method.py
+++ PE_Max_Prime_Alternative_Tree_Method.py
@@ -1,13 +1,17 @@
+# This method uses a different iteration
+# approach to solving the maximum prime
+# number calculator
+
 num = 46337
 newNum = num
 largestFactor = 0
 counter = 2
 
-while counter * counter <= newNum:
-    if (newNum % counter == 0):
+while counter * counter <= newNum: # denotes the square counter comparison
+    if (newNum % counter == 0): # verifies if it is not prime
         newNum = newNum / counter
         largestFactor = counter
-    else:
+    else: # it is a prime factor
         counter += 1
 if newNum > largestFactor:
     largestFactor = newNum
