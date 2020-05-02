760cf135e0e6a86575c272f73536f73f03388f21
jnwiggins@sdsu.edu <jnwiggins@sdsu.edu>
2020-05-01 22:55:31 -0700

    Learned about lists
    Learned the feature of list recently where it can parse a word into its
    individiual characters.
    This is a useful skill when dealing with numeric pallendromes and more.
    Saved this skill for later use.

---
+++ list_features.py
@@ -0,0 +1,9 @@
+word = 'supercalifragalisticexpialidotious'
+
+print('The length of the word is:', len(word))
+
+letters = list(word)
+
+print('The word has', len(letters), 'letters.')
+
+print(word, letters)
